aeb858a08995fe94af14135515d11a969c240b9e
tomo <49612544+tomo2403@users.noreply.github.com>
2025-06-19 10:55:27 +0200

    testing gpio

--- src/main.c
+++ src/main.c
@@ -1,6 +1,61 @@
-#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
 
-void app_main() {
-    printf("Hello, ESP32-S3!\n");
-    // Add your application logic here
+#define IN1 GPIO_NUM_1
+#define IN2 GPIO_NUM_2
+#define IN3 GPIO_NUM_42
+#define IN4 GPIO_NUM_41
+
+void app_main(void)
+{
+    // Pins als Output setzen
+    gpio_set_direction(IN1, GPIO_MODE_OUTPUT);
+    gpio_set_direction(IN2, GPIO_MODE_OUTPUT);
+    gpio_set_direction(IN3, GPIO_MODE_OUTPUT);
+    gpio_set_direction(IN4, GPIO_MODE_OUTPUT);
+
+    while (1)
+    {
+        gpio_set_level(IN1, 1);
+    }
+
+    // while (1)
+    // {
+    //     // Motor A im Uhrzeigersinn
+    //     gpio_set_level(IN1, 1);
+    //     gpio_set_level(IN2, 0);
+    //     vTaskDelay(pdMS_TO_TICKS(2000));
+    //
+    //     gpio_set_level(IN1, 1);
+    //     gpio_set_level(IN2, 1);
+    //     vTaskDelay(pdMS_TO_TICKS(500));
+    //
+    //     // Motor B im Uhrzeigersinn
+    //     gpio_set_level(IN3, 1);
+    //     gpio_set_level(IN4, 0);
+    //     vTaskDelay(pdMS_TO_TICKS(2000));
+    //
+    //     gpio_set_level(IN3, 1);
+    //     gpio_set_level(IN4, 1);
+    //     vTaskDelay(pdMS_TO_TICKS(500));
+    //
+    //     // Motor A gegen Uhrzeigersinn
+    //     gpio_set_level(IN1, 0);
+    //     gpio_set_level(IN2, 1);
+    //     vTaskDelay(pdMS_TO_TICKS(2000));
+    //
+    //     gpio_set_level(IN1, 1);
+    //     gpio_set_level(IN2, 1);
+    //     vTaskDelay(pdMS_TO_TICKS(500));
+    //
+    //     // Motor B gegen Uhrzeigersinn
+    //     gpio_set_level(IN3, 0);
+    //     gpio_set_level(IN4, 1);
+    //     vTaskDelay(pdMS_TO_TICKS(2000));
+    //
+    //     gpio_set_level(IN3, 1);
+    //     gpio_set_level(IN4, 1);
+    //     vTaskDelay(pdMS_TO_TICKS(500));
+    // }
 }
