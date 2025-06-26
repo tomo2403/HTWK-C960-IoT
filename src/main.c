d49d12738f1e5e1caee47a457330f246b4b63de9
tomo <49612544+tomo2403@users.noreply.github.com>
2025-06-26 13:21:49 +0200

    implemented connection wifi

--- src/main.c
+++ src/main.c
@@ -1,61 +1,20 @@
 #include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "driver/gpio.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include <string.h>
+#include "../include/wlan.h"
 
-#define IN1 GPIO_NUM_1
-#define IN2 GPIO_NUM_2
-#define IN3 GPIO_NUM_42
-#define IN4 GPIO_NUM_41
+static const char *TAG = "wifi_sta";
 
 void app_main(void)
 {
-    // Pins als Output setzen
-    gpio_set_direction(IN1, GPIO_MODE_OUTPUT);
-    gpio_set_direction(IN2, GPIO_MODE_OUTPUT);
-    gpio_set_direction(IN3, GPIO_MODE_OUTPUT);
-    gpio_set_direction(IN4, GPIO_MODE_OUTPUT);
+	vTaskDelay(1000 / portTICK_PERIOD_MS); // Wartezeit für NVS-Initialisierung
+	nvs_flash_init();
 
-    while (1)
-    {
-        gpio_set_level(IN1, 1);
-    }
+	initStaEventHandlers();
+	initSTA();
 
-    // while (1)
-    // {
-    //     // Motor A im Uhrzeigersinn
-    //     gpio_set_level(IN1, 1);
-    //     gpio_set_level(IN2, 0);
-    //     vTaskDelay(pdMS_TO_TICKS(2000));
-    //
-    //     gpio_set_level(IN1, 1);
-    //     gpio_set_level(IN2, 1);
-    //     vTaskDelay(pdMS_TO_TICKS(500));
-    //
-    //     // Motor B im Uhrzeigersinn
-    //     gpio_set_level(IN3, 1);
-    //     gpio_set_level(IN4, 0);
-    //     vTaskDelay(pdMS_TO_TICKS(2000));
-    //
-    //     gpio_set_level(IN3, 1);
-    //     gpio_set_level(IN4, 1);
-    //     vTaskDelay(pdMS_TO_TICKS(500));
-    //
-    //     // Motor A gegen Uhrzeigersinn
-    //     gpio_set_level(IN1, 0);
-    //     gpio_set_level(IN2, 1);
-    //     vTaskDelay(pdMS_TO_TICKS(2000));
-    //
-    //     gpio_set_level(IN1, 1);
-    //     gpio_set_level(IN2, 1);
-    //     vTaskDelay(pdMS_TO_TICKS(500));
-    //
-    //     // Motor B gegen Uhrzeigersinn
-    //     gpio_set_level(IN3, 0);
-    //     gpio_set_level(IN4, 1);
-    //     vTaskDelay(pdMS_TO_TICKS(2000));
-    //
-    //     gpio_set_level(IN3, 1);
-    //     gpio_set_level(IN4, 1);
-    //     vTaskDelay(pdMS_TO_TICKS(500));
-    // }
+	// TODO: MQTT
 }
