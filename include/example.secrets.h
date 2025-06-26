76fa4bbbd1f0812d2f615fb16dba81ed91c91ec0
tomo <49612544+tomo2403@users.noreply.github.com>
2025-06-26 13:21:00 +0200

    added secret template

---
+++ include/example.secrets.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// Copy to secrets.h and fill in your credentials
+
+#define CONFIG_WPA2E_SSID "eduroam"
+#define CONFIG_WPA2E_IDENTITY "eduroam@htwk-leipzig.de"
+#define CONFIG_WPA2E_USERNAME ""
+#define CONFIG_WPA2E_PASSWORD ""
+
+#define CONFIG_WPA2_SSID ""
+#define CONFIG_WPA2_PASSWORD ""
