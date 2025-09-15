ad30ee9db340f83ccf3d9ecb5c9fe9416010e1a7
Gregor Knoche <gregor.knoche@stud.htwk-leipzig.de>
2025-09-15 14:09:51 +0200

    timer implemented

--- src/main.c
+++ src/main.c
@@ -13,6 +13,9 @@
 #include "espnow.h"
 #include "joystick.h"
 #include "motor.h"
+#include "driver/gpio.h"
+#include "driver/timer.h"
+#include "esp_intr_alloc.h"
 
 #define DISABLE_HUMIDITY true
 
@@ -375,6 +378,37 @@ void postSensorData(void *args)
 
         ESP_LOGD(TAG, "TVOC: %s,  eCO2: %s, Temp: %s, Pres: %s, Hum: %s", tvoc_buf, eco2_buf, temp_buf, pres_buf, hum_buf);
     }
+}
+
+void IRAM_ATTR timer_isr(void* arg) {
+    static bool led_on = false;
+    led_on = !led_on;
+    gpio_set_level(6, led_on);
+    gpio_set_level(7, !led_on);
+
+    // Interrupt-Flag zurücksetzen
+    TIMERG0.int_clr_timers.t0 = 1;
+    // Timer wieder starten (reload)
+    TIMERG0.hw_timer[0].config.alarm_en = TIMER_ALARM_EN;
+}
+
+void setup_hw_timer_led() {
+    gpio_set_direction(6, GPIO_MODE_OUTPUT);
+    gpio_set_direction(7, GPIO_MODE_OUTPUT);
+
+    timer_config_t config = {
+        .divider = 8000, // 80 MHz / 8000 = 10 kHz
+        .counter_dir = TIMER_COUNT_UP,
+        .counter_en = TIMER_PAUSE,
+        .alarm_en = TIMER_ALARM_EN,
+        .auto_reload = true,
+    };
+    timer_init(TIMER_GROUP_0, TIMER_0, &config);
+    timer_set_counter_value(TIMER_GROUP_0, TIMER_0, 0);
+    timer_set_alarm_value(TIMER_GROUP_0, TIMER_0, 5000); // 500ms (10kHz * 0.5s)
+    timer_enable_intr(TIMER_GROUP_0, TIMER_0);
+    timer_isr_register(TIMER_GROUP_0, TIMER_0, timer_isr, NULL, ESP_INTR_FLAG_IRAM, NULL);
+    timer_start(TIMER_GROUP_0, TIMER_0);
 }
 
 void app_main(void)
